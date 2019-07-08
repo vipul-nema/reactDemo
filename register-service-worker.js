623c4465f56314919f5739f659134bd692a1a59e
Vipul Nema <vipul@naurki.com>
2019-07-08 23:05:43 +0530

    service worker path updated

--- register-service-worker.js
+++ register-service-worker.js
@@ -1,14 +1,16 @@
 // ServiceWorker is a progressive technology. Ignore unsupported browsers
 if ("serviceWorker" in navigator) {
   console.log("CLIENT: service worker registration in progress.");
-  navigator.serviceWorker.register("./service-worker.js").then(
-    function() {
-      console.log("CLIENT: service worker registration complete.");
-    },
-    function() {
-      console.log("CLIENT: service worker registration failure.");
-    }
-  );
+  navigator.serviceWorker
+    .register("https://vipul-nema.github.io/reactDemo/service-worker.js")
+    .then(
+      function() {
+        console.log("CLIENT: service worker registration complete.");
+      },
+      function() {
+        console.log("CLIENT: service worker registration failure.");
+      }
+    );
 } else {
   console.log("CLIENT: service worker is not supported.");
 }
