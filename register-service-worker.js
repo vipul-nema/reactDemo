6826beb2d29450a9ca94cb1318b5ff50e704b763
Vipul Nema <vipul@naurki.com>
2019-07-21 23:13:05 +0530

    service worker self.skipWaiting and clients.calim method added

--- register-service-worker.js
+++ register-service-worker.js
@@ -1,16 +1,21 @@
 // ServiceWorker is a progressive technology. Ignore unsupported browsers
 if ("serviceWorker" in navigator) {
   console.log("CLIENT: service worker registration in progress.");
-  navigator.serviceWorker
-    .register("https://vipul-nema.github.io/reactDemo/service-worker.js")
-    .then(
-      function() {
-        console.log("CLIENT: service worker registration complete.");
-      },
-      function() {
-        console.log("CLIENT: service worker registration failure.");
-      }
-    );
+  navigator.serviceWorker.register("https://vipul-nema.github.io/reactDemo/service-worker.js").then(
+    function() {
+      console.log("CLIENT: service worker registration complete.");
+    },
+    function() {
+      console.log("CLIENT: service worker registration failure.");
+    }
+  );
+
+  navigator.serviceWorker.addEventListener("controllerchange", () => {
+    // This fires when the service worker controlling this page
+    // changes, eg a new worker has skipped waiting and become
+    // the new active worker.
+    console.log("service worker controlling this page has changed and updated");
+  });
 } else {
   console.log("CLIENT: service worker is not supported.");
 }
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,10 @@
-var currentCacheName = "sw-demo_v1";
+var currentCacheName = "sw-demo_v4";
 self.addEventListener("install", function(event) {
+  //self.skipWaiting() prevents the waiting, meaning the service worker activates as soon as it's finished installing.
+  //This means some of your page's fetches will have been handled by your old service worker,
+  // but your new service worker will be handling subsequent fetches.
+  //If this might break things, don't use skipWaiting().
+  self.skipWaiting();
   console.log("WORKER: install event in progress.");
 });
 
@@ -20,16 +25,34 @@ self.addEventListener("activate", function(event) {
       );
     })
   );
+  //You can take control of uncontrolled clients by calling clients.claim()
+  // within your service worker once it's activated.
+  clients.claim();
 });
 
 self.addEventListener("fetch", function(event) {
   console.log("WORKER: fetch event in progress.", event.request.url);
+  var request = event.request;
   event.respondWith(
     caches.open(currentCacheName).then(function(cache) {
       return cache.match(event.request).then(function(response) {
+        var corsRequest;
+        if (request.mode !== "cors") {
+          corsRequest = new Request(request, {
+            mode: "cors",
+            credentials: "omit" // include, *same-origin, omit
+          });
+        } else {
+          // Clone the request. A request is a stream and
+          // can only be consumed once. Since we are consuming this
+          // once by cache and once by the browser for fetch, we need
+          // to clone the request.
+          corsRequest = request.clone();
+        }
+
         return (
           response ||
-          fetch(event.request).then(function(response) {
+          fetch(corsRequest).then(function(response) {
             cache.put(event.request, response.clone());
             return response;
           })
