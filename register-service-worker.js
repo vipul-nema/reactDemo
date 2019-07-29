7279ea6431a9626f717f1a7da759e534fc88f5fc
Vipul Nema <vipul@naurki.com>
2019-07-29 11:26:41 +0530

    register through script src...

--- register-service-worker.js
+++ register-service-worker.js
@@ -1,7 +1,7 @@
 // ServiceWorker is a progressive technology. Ignore unsupported browsers
 if ("serviceWorker" in navigator) {
   console.log("CLIENT: service worker registration in progress.");
-  navigator.serviceWorker.register("https://vipul-nema.github.io/reactDemo/service-worker.js").then(
+  navigator.serviceWorker.register("./service-worker.js").then(
     function() {
       console.log("CLIENT: service worker registration complete.");
     },
